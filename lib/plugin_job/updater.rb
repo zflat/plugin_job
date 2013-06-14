3266b7222d4470b8cc9a72ac6c61f0ed59bcffef
William Wedler <wwedler@riseup.net>
2013-06-14 04:31:41 -0400

    cleanup unused code

--- lib/plugin_job/updater.rb
+++ lib/plugin_job/updater.rb
@@ -1,14 +1,8 @@
-require "eventmachine"
-
 module PluginJob
   module Updater
     def self.update
       require 'bundler/cli'
       args = ['update', PluginJob.configuration.base_gem]
-      if false && PluginJob.configuration.gemfile_path.length > 0
-        path_arg = "--path=#{PluginJob.configuration.gemfile_path}"
-        args << path_arg
-      end
       Bundler::CLI.start(args)
     end
 
